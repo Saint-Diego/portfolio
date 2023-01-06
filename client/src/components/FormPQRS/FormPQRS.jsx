a0c002261d4b06a8d7b7669fe10e988a26ec45e6
Diego López Beltrán <ing.diegolopezb@gmail.com>
2023-01-06 12:28:16 -0500

    responsive navbar

--- client/src/components/FormPQRS/FormPQRS.jsx
+++ client/src/components/FormPQRS/FormPQRS.jsx
@@ -7,7 +7,7 @@ const FormPQRS = () => {
   };
 
   return (
-    <div className="container">
+    <div className="container container-heigth">
       <form className="form-control my-5" onSubmit={handleSubmit}>
         <div className="form-group my-3">
           <input className="form-control" type="text" placeholder="Ingrese nombre" />
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,36 +1,82 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
-import InfoIcon from "@mui/icons-material/Info";
-import MoreIcon from "@mui/icons-material/MoreHoriz";
-import ProjectsIcon from "@mui/icons-material/AccountTree";
 import "./NavBar.css";
 import CustomizedSwitches from "../CustomizedSwitches/CustomizedSwitches.jsx";
 
 const NavBar = () => {
   
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <NavLink to="/" className="link align-middle mx-3 p-2">
-          <HomeIcon />
-        </NavLink>
-        <div className="col-5 d-flex justify-content-end">
-          <NavLink to="/projects" className="link align-middle mx-3 p-2">
-            <span className="fw-bold">Proyectos</span>
-            <ProjectsIcon />
+    // <nav className="navbar navbar-expand-lg">
+    //   <div className="container-fluid">
+    //     <NavLink to="/" className="link align-middle mx-3 p-2">
+    //       <HomeIcon />
+    //     </NavLink>
+    //     <div className="col-5 d-flex justify-content-end">
+    //       <NavLink to="/projects" className="link align-middle mx-3 p-2">
+    //         <span className="fw-bold">Proyectos</span>
+    //         <ProjectsIcon />
+    //       </NavLink>
+    //       <NavLink to="/detail" className="link align-middle mx-3 p-2">
+    //         <span className="fw-bold">Resumen</span>
+    //         <MoreIcon />
+    //       </NavLink>
+    //       <NavLink to="/about" className="link align-middle mx-3 p-2">
+    //         <span className="fw-bold">Acerca de</span>
+    //         <InfoIcon />
+    //       </NavLink>
+    //       <CustomizedSwitches />
+    //     </div>
+    //   </div>
+    // </nav>
+    <Navbar bg="light" expand="lg">
+      <Container className='border border-1 border-success'>
+        <Navbar.Brand className='border border-primary'>
+          <NavLink to="/" className="link align-middle mx-3 p-2">
+            <HomeIcon />
           </NavLink>
-          <NavLink to="/detail" className="link align-middle mx-3 p-2">
-            <span className="fw-bold">Resumen</span>
-            <MoreIcon />
-          </NavLink>
-          <NavLink to="/about" className="link align-middle mx-3 p-2">
-            <span className="fw-bold">Acerca de</span>
-            <InfoIcon />
-          </NavLink>
-          <CustomizedSwitches />
-        </div>
-      </div>
-    </nav>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className='border border-warning text-end' id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link>
+              <NavLink to="/" className="link align-middle mx-3 p-2">
+                Home
+              </NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              <NavLink to="/projects" className="link align-middle mx-3 p-2">
+                <span className="fw-bold">Proyectos</span>
+              </NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              <NavLink to="/detail" className="link align-middle mx-3 p-2">
+                <span className="fw-bold">Resumen</span>
+              </NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              <NavLink to="/about" className="link align-middle mx-3 p-2">
+                <span className="fw-bold">Acerca de</span>
+              </NavLink>
+            </Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
